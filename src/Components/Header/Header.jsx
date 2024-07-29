16bb19d1c0578671a5209ea87748f09074bfa856
PolouX <deoh02@gmail.com>
2024-07-29 12:40:21 -0600

    sidebar + login

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="Header">
+        <h1>Usuarios</h1>
+        <p>Crea,edita o consulta la lista de usuarios</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/Admin/Usuarios/Usuarios.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Usuarios.css"
+
+const Usuarios = () => {
+  return (
+    <div className="usuarios">
+        
+    </div>
+  )
+}
+
+export default Usuarios
