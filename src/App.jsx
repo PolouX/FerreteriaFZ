1889920a7ef49faf945b9a3d9f3ec6b91a413d4f
DiegoMTZ923 <A01634562@tec.mx>
2024-08-06 14:29:06 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
+import PrivateRoute from './PrivateRoute';
 import Login from './Pages/Login/Login';
 import Usuarios from './Pages/Admin/Usuarios/Usuarios';
 import Pedidos from './Pages/Admin/Pedidos/Pedidos';
@@ -7,32 +9,29 @@ import Estadisticas from './Pages/Admin/Estadisticas/Estadisticas';
 import Clientes from './Pages/Admin/Clientes/Clientes';
 import Inventario from './Pages/Admin/Inventario/Inventario';
 import Credito from './Pages/Admin/Credito/Credito';
-import PrivateRoute from './PrivateRoute';
-import { AuthProvider } from './AuthContext';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Header from './Components/Header/Header';
-import "./App.css"
+
+const Layout = () => {
+  return (
+    <div className="app_layout">
+      <Sidebar />
+      <Header />
+      <div className="app_content">
+        <Routes>
+          <Route path="usuarios" element={<Usuarios />} />
+          <Route path="pedidos" element={<Pedidos />} />
+          <Route path="estadisticas" element={<Estadisticas />} />
+          <Route path="clientes" element={<Clientes />} />
+          <Route path="inventario" element={<Inventario />} />
+          <Route path="credito" element={<Credito />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
-  const Layout = () => {
-    return (
-      <div className="app_layout">
-        <Sidebar />
-        <Header />
-        <div className="app_content">
-          <Routes>
-            <Route path="usuarios" element={<Usuarios />} />
-            <Route path="pedidos" element={<Pedidos />} />
-            <Route path="estadisticas" element={<Estadisticas />} />
-            <Route path="clientes" element={<Clientes />} />
-            <Route path="inventario" element={<Inventario />} />
-            <Route path="credito" element={<Credito />} />
-          </Routes>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <AuthProvider>
       <Router>
