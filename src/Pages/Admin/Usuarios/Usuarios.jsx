339536e9b65de3b6d606fdb7ad817f42bdbd8b51
PolouX <deoh02@gmail.com>
2024-08-06 12:00:33 -0600

    Tabla inicial de usuarios

--- src/Pages/Admin/Usuarios/Usuarios.jsx
+++ src/Pages/Admin/Usuarios/Usuarios.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import "./Usuarios.css"
+import React, {useState, useEffect} from 'react'
+import "./Usuarios.css";
+import { db } from '../../../firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Usuarios = () => {
-  return (
-    <div className="usuarios">
-        
-    </div>
-  )
-}
+  // useState para mantener los usuarios obtenidos de la base de datos
+  const [users, setUsers] = useState([]);
 
-export default Usuarios
+  // useEffect para obtener los datos cuando el componente se monta
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usuarios = collection(db, 'usuarios');
+      const querySnapshot = await getDocs(usuarios);
+      const usersList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setUsers(usersList);
+    };
+
+    fetchUsers();
+  }, []); // El array vacío [] asegura que el efecto solo se ejecute una vez al montar el componente
+
+  return (
+    <table className='usuarios-table-container'>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Rol</th>
+          <th>Contraseña</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.nombre}</td>
+            <td>{user.apellido}</td>
+            <td>{user.permisos}</td>
+            <td>{user.contrasena}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Usuarios;
