7bde4fb4365ab33da88ed17c5ae325c1237cd92e
PolouX <deoh02@gmail.com>
2024-08-19 14:08:05 -0600

    clientes iniciado

--- src/Components/Clientes/Clientes.jsx
+++ src/Components/Clientes/Clientes.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Clientes.css"
 
 const Clientes = () => {
   return (
     <>
+      <div className="clientes-header">
+        <div className="clientes-left-filters">
+          <button>Activos</button>
+          <button>Facturas</button>
+        </div>
+        <div className="right-filters">
+          <input type="text" placeholder='Buscar un pedido...'/>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+      <div className="clientes-content">
+      </div>
     </>
   )
 }
