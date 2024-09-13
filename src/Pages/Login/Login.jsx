2447850bc00e478205857d8d98d8236bfce68056
DiegoMTZ923 <A01634562@tec.mx>
2024-09-13 10:36:26 -0600

    .

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -8,7 +8,7 @@ import AuthContext from '../../AuthContext';
 
 const Login = () => {
   const navigate = useNavigate();
-  const { setIsAuthenticated, setUser } = useContext(AuthContext);  // Añadir setUser al contexto
+  const { setIsAuthenticated, setUser } = useContext(AuthContext); 
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState("");
 
@@ -32,12 +32,12 @@ const Login = () => {
         setError("¡Contraseña incorrecta!");
       } else {
         const doc = querySnapshot.docs[0];
-        const userData = doc.data();  // Obtén la información del usuario
+        const userData = doc.data();
         setError("");
         setIsAuthenticated(true);
-        setUser(userData);  // Almacena la información del usuario en el contexto
+        setUser(userData); 
 
-        // Redirigir basado en el rol del usuario
+     
         if (userData.permisos.includes('Admin')) {
           navigate("/admin/usuarios");
         } else if (userData.permisos.includes('credito')) {
