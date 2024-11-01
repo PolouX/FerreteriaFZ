28bd6229784f4dc35ac2089d030016cde5f14520
DiegoMTZ923 <A01634562@tec.mx>
2024-11-01 12:32:05 -0600

    API KEYS

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -4,18 +4,26 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+// Log para verificar las variables de entorno
+console.log("API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("Auth Domain:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("Storage Bucket:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log("Messaging Sender ID:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+console.log("App ID:", import.meta.env.VITE_FIREBASE_APP_ID);
+console.log("Measurement ID:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);  // Inicializar Firebase Storage
+const storage = getStorage(app);
 
-export { db, storage };  // Exportar también storage
-
+export { db, storage };
